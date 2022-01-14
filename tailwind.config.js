d376cc0eb8c5d29ccd42e6a16b65ba776944239b
umutyerebakmaz <umutyerebakmaz@gmail.com>
2022-01-14 17:03:29 +0300

    tailwind 3.0 config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ const { guessProductionMode } = require("@ngneat/tailwind");
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-      content: [
-        './src/**/*.{html,ts,css,scss,sass,less,styl}',
-      ]
-    },
-    darkMode: false, // or 'media' or 'class'
+  prefix: '',
+  mode: 'jit',
+  content: [
+    './src/**/*.{html,ts,css,scss,sass,less,styl}'
+  ],
     theme: {
       extend: {},
     },
     variants: {
       extend: {},
     },
-    plugins: [require('@tailwindcss/aspect-ratio'),require('@tailwindcss/forms'),require('@tailwindcss/line-clamp'),require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography')
+  ],
 };
